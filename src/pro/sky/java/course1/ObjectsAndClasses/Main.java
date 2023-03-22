27ad49c877572643676e57db31a39b70126ec38c
action0709 <action0709@mail.ru>
2023-03-22 17:21:21 +0400

    1-7

--- src/pro/sky/java/course1/ObjectsAndClasses/Main.java
+++ src/pro/sky/java/course1/ObjectsAndClasses/Main.java
@@ -2,12 +2,14 @@ package pro.sky.java.course1.ObjectsAndClasses;
 
 public class Main {
     public static void main(String[] args) {
-        Book BookGarry = new Book("Гарри Поттер и узник Азкабана","Джоан Роулинг",2019);
-        System.out.println("BookGarry = " + BookGarry.getTitleBook()+", "+BookGarry.getAutor()+", "+BookGarry.getYearPublication());
-        Book BookKing = new Book("Зелена миля", "Стивен Кинг", 2014);
-        System.out.println("BookKing = " + BookKing.getAutor()+", "+BookKing.getTitleBook()+", "+BookKing.getYearPublication());
+        Autor Rouling = new Autor("Джоан", "Роулинг");
+        System.out.println("Rouling = " + Rouling.getName()+" "+Rouling.getSurname());
         Autor King = new Autor("Стивен", "Кинг");
-        System.out.println("King = " + King.getName()+King.getSurname());
+        System.out.println("King = " + King.getName()+" "+King.getSurname());
+        Book BookGarry = new Book("Гарри Поттер и узник Азкабана", Rouling.getName()+" "+Rouling.getSurname(), 2019);
+        System.out.println("BookGarry = " + BookGarry.getTitleBook()+", "+BookGarry.getAutor()+", "+BookGarry.getYearPublication());
+        Book BookKing = new Book("Зелена миля", King.getName()+" "+ King.getSurname(), 2014);
+        System.out.println("BookKing = " + BookKing.getTitleBook()+", "+BookKing.getAutor()+", "+BookKing.getYearPublication());
         BookGarry.setYearPublication(2020);
         System.out.println("BookGarry = " + BookGarry.getTitleBook()+", "+BookGarry.getAutor()+", "+BookGarry.getYearPublication());
 
